13cd0abf1c306cfec857e22d561a9b558b172911
Mehthab <114091862+Meht4b@users.noreply.github.com>
2024-08-10 23:29:36 +0530

    added script

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById('messageBoxButton').onclick=submitFunction;
+
+function submitFunction(){
+
+    const message = document.getElementById('messageBoxInput').value;
+    document.getElementById('messageBoxInput').value='';
+    const liChild = document.createElement('li');
+
+    const senderName = document.createElement('p');
+    senderName.innerHTML='person 1';
+    const senderMessage = document.createElement('h3');
+    senderMessage.innerHTML=message;
+
+    liChild.appendChild(senderName)
+    liChild.appendChild((senderMessage))
+    liChild.classList.add('textBox')
+
+    document.getElementById('messages').appendChild(liChild)
+
+
+}
